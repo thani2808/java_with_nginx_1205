013573339386233a4ec115f47967e1fb702c48f7
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-05-13 14:28:35 +0530

    updated files

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 provider "aws" {
   region = var.region
-  #access_key = var.access_key
-  #secret_key = var.secret_key
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 module "vpc" {
@@ -11,20 +13,13 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = var.private_subnets
-  private_subnet_names = var.private_subnet_names
-  public_subnets       = var.public_subnets
-  public_subnet_names  = var.public_subnet_names
+  azs                 = data.aws_availability_zones.available.names
+  public_subnets      = var.public_subnets
+  public_subnet_names = var.public_subnet_names
 
   public_subnet_tags = {
     subnet                   = "public"
     "kubernetes.io/role/elb" = "1"
-  }
-
-  private_subnet_tags = {
-    subnet                            = "private"
-    "kubernetes.io/role/internal-elb" = "1"
   }
 
   tags = {
@@ -32,114 +27,6 @@ module "vpc" {
     Environment = "dev"
   }
 }
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-resource "aws_security_group" "dev" {
-  vpc_id = module.vpc.vpc_id
-
-  # Define ingress and egress rules
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Adjust as necessary
-    description = "Allow SSH"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
-    description = "Allow all outbound traffic"
-  }
-
-  tags = {
-    Name = "dev Security Group"
-  }
-}
-
-# resource "aws_instance" "dev_ec2_public" {
-#   count                       = length(module.vpc.public_subnets)
-#   ami                         = var.ami
-#   instance_type               = var.instance_type
-#   subnet_id                   = module.vpc.public_subnets[count.index]
-#   key_name                    = var.key_name
-#   vpc_security_group_ids      = [aws_security_group.dev.id]
-#   associate_public_ip_address = true # 🔑 Needed to get public IP
-
-#   tags = {
-#     Name = "EC2-${count.index}"
-#   }
-
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sleep 30",
-#       "touch hello.txt",
-#       "echo helloworld remote provisioner >> hello.txt",
-#     ]
-#   }
-#   connection {
-#     type        = "ssh"
-#     host        = self.public_ip
-#     user        = "ubuntu"
-#     private_key = file("/home/thani/.ssh/id_rsa")
-#     timeout     = "12m"
-#   }
-# }
-
-resource "aws_instance" "dev_ec2_private" {
-  count                  = length(module.vpc.private_subnets)
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = module.vpc.private_subnets[count.index]
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.private_ec2.id]
-
-  tags = {
-    Name = "EC2-${count.index}"
-  }
-
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "sleep 30",
-  #     "touch hello.txt",
-  #     "echo helloworld remote provisioner >> hello.txt",
-  #   ]
-  # }
-
-  depends_on = [aws_instance.bastion]
-
-  connection {
-    type                = "ssh"
-    host                = self.private_ip
-    user                = "ubuntu"
-    private_key         = file("/home/thani/.ssh/id_rsa_555")
-    bastion_host        = aws_instance.bastion.public_ip
-    bastion_user        = "ubuntu"
-    bastion_private_key = file("/home/thani/.ssh/id_rsa_555")
-    timeout             = "12m"
-  }
-}
-
-
-# locals {
-#   ingress_rules = [{
-#     port        = 80
-#     description = "Ingress rules for port 80"
-#     },
-#     {
-#       port        = 3389
-#       description = "Ingress rules for port 3389"
-#     },
-#     {
-#       port        = 22
-#       description = "Ingress rules for port 22"
-#   }]
-# }
 
 resource "aws_security_group" "ansible_bastion_sg" {
   vpc_id      = module.vpc.vpc_id
@@ -151,7 +38,7 @@ resource "aws_security_group" "ansible_bastion_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = var.cidr_blocks_ingress_bastion
-    description = "Allow SSH from my IP to Bastion"
+    description = "Allow SSH from trusted IPs"
   }
 
   egress {
@@ -179,88 +66,15 @@ resource "aws_instance" "bastion" {
               #!/bin/bash
               apt-get update -y
               apt-get install -y software-properties-common git vim
-              
               add-apt-repository --yes --update ppa:ansible/ansible
               apt install -y ansible >> /var/log/ansible-install.log 2>&1
               echo "Ansible installed successfully" >> /var/log/ansible-install.log
-
-              # Install Git
-              apt install -y git
-
-              # Clone the Git repository (replace with your repo URL)
               git clone https://github.com/thani2808/java_with_nginx_1205.git /opt/ansible-playbooks
-
-              echo "Git repo cloned successfully" >> /var/log/ansible-install.log              
+              echo "Git repo cloned successfully" >> /var/log/ansible-install.log
               EOF
 
   tags = {
-    Name = "Bastion"
+    Name        = "Bastion"
+    Environment = "dev"
   }
 }
-
-
-resource "aws_security_group" "private_ec2" {
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ansible_bastion_sg.id]
-    description     = "Allow SSH from Bastion"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = var.cidr_blocks_egress
-    description = "Allow all outbound traffic"
-  }
-
-  tags = {
-    Name = "Private-EC2-Security-Group"
-  }
-}
-
-# resource "aws_key_pair" "deployer" {
-#   key_name   = var.key_name
-#   public_key = var.public_key
-# }
-
-# resource "aws_security_group" "firs_windows_image_sg" {
-#   name        = "firs_windows_image_sg"
-#   vpc_id      = module.vpc.vpc_id
-#   description = "Allow RDP"
-#   ingress {
-#     from_port   = 3389
-#     to_port     = 3389
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] # You should restrict this in production!
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-# resource "aws_instance" "firs_windows_image_instance" {
-#   ami                         = "ami-0907008e2c2a9e429" # Example Windows AMI (adjust this!)
-#   instance_type               = "t3.micro"
-#   key_name                    = var.my-windows-keys
-#   subnet_id                   = module.vpc.public_subnets[0]
-#   vpc_security_group_ids      = [aws_security_group.firs_windows_image_sg.id]
-#   associate_public_ip_address = true
-
-#   tags = {
-#     Name = "WindowsAMIBase"
-#   }
-# }
-
-# resource "aws_ami_from_instance" "windows_ami" {
-#   name               = "firs_image_ami"
-#   source_instance_id = aws_instance.firs_windows_image_instance.id
-#   #depends_on         = [aws_instance.firs-windows_image_instance]
-# }
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,7 @@
-output "private_subnet_id" {
-  value = module.vpc.private_subnets
-}
-
-output "public_subnet_id" {
+output "public_subnet_ids" {
   value = module.vpc.public_subnets
 }
 
-output "IGW_id" {
+output "igw_id" {
   value = module.vpc.igw_id
 }
-
-# output "windows_instance_id" {
-#   value = aws_instance.firs_windows_image_instance.id
-# }
